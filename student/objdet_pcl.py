b0db20ceadf09c7697e7523f23b95b20d7fe1dc0
Atanasko Boris Mitrev <atanasko.mitrev@gmail.com>
2022-03-22 14:48:17 +0100

    Update show_pcl implementation

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -41,9 +41,13 @@ def load_range_image(frame, lidar_name):
         ri = np.array(ri.data).reshape(ri.shape.dims)
     return ri
 
+
 def key_callback(vis):
     vis.destroy_window()
 
+
+vis = o3d.visualization.VisualizerWithKeyCallback()
+first_frame = True
 # visualize lidar point-cloud
 def show_pcl(pcl):
 
@@ -52,29 +56,27 @@ def show_pcl(pcl):
     print("student task ID_S1_EX2")
 
     # step 1 : initialize open3d with key callback and create window
-    vis = o3d.visualization.Visualizer()
-    if not hasattr(show_pcl, "cnt_frame"):
-        show_pcl.cnt_frame = 0
-        # vis.register_key_callback(262, key_callback)
-        vis.create_window(width=640, height=480)
+    global first_frame
+    if first_frame:
+        vis.create_window()
+        vis.register_key_callback(262, key_callback)
 
     # step 2 : create instance of open3d point-cloud class
     pcd = o3d.geometry.PointCloud()
 
     # step 3 : set points in pcd instance by converting the point-cloud into 3d vectors (using open3d function Vector3dVector)
-    pcd.points = o3d.utility.Vector3dVector(pcl)
+    pcd.points = o3d.utility.Vector3dVector(pcl[:, :3])
 
     # step 4 : for the first frame, add the pcd instance to visualization using add_geometry; for all other frames, use update_geometry instead
-    if show_pcl.cnt_frame == 0:
+    if first_frame:
         vis.add_geometry(pcd)
+        first_frame = False
     else:
         vis.update_geometry(pcd)
 
     # step 5 : visualize point cloud and keep window open until right-arrow is pressed (key-code 262)
-    # vis.draw_geometries([pcd])
+    vis.poll_events()
     vis.update_renderer()
-
-    show_pcl.cnt_frame += 1
 
     #######
     ####### ID_S1_EX2 END #######     
