b9c8bb8757e57c306923e63003215929a4b3001b
Atanasko Boris Mitrev <atanasko.mitrev@gmail.com>
2022-03-24 01:07:13 +0100

    ID_S3_EX2 Extract 3D bounding boxes from model response

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -201,16 +201,6 @@ def detect_objects(input_bev_maps, model, configs):
             ####### ID_S3_EX1-5 START #######     
             #######
             print("student task ID_S3_EX1-5")
-            # outputs = model(input_bev_maps)
-            # outputs['hm_cen'] = _sigmoid(outputs['hm_cen'])
-            # outputs['cen_offset'] = _sigmoid(outputs['cen_offset'])
-            # detections = decode(outputs['hm_cen'], outputs['cen_offset'], outputs['direction'], outputs['z_coor'],
-            #                     outputs['dim'], K=configs.K)
-            # detections = detections.cpu().numpy().astype(np.float32)
-            # # detections = post_processing(detections, configs.num_classes, configs.down_ratio, configs.peak_thresh)
-            # detections = post_processing(detections, configs)
-            # detections = detections[0]  # only first batch
-
             outputs = model(input_bev_maps)
             outputs['hm_cen'] = _sigmoid(outputs['hm_cen'])
             outputs['cen_offset'] = _sigmoid(outputs['cen_offset'])
@@ -234,12 +224,24 @@ def detect_objects(input_bev_maps, model, configs):
     objects = [] 
 
     ## step 1 : check whether there are any detections
+    # if np.size(detections) > 0:
+    if len(detections) > 0:
 
         ## step 2 : loop over all detections
+        for obj in detections:
         
             ## step 3 : perform the conversion using the limits for x, y and z set in the configs structure
+            _id, _x, _y, _z, _h, _w, _l, _yaw = obj
+
+            x = (_y - configs.lim_y[0]) / (configs.lim_y[1] - configs.lim_y[0]) * configs.bev_width
+            y = (_x - configs.lim_x[0]) / (configs.lim_x[1] - configs.lim_x[0]) * configs.bev_height
+            z = _z - configs.lim_z[0]
+            w = _w / (configs.lim_y[1] - configs.lim_y[0]) * configs.bev_width
+            l = _l / (configs.lim_x[1] - configs.lim_x[0]) * configs.bev_height
+            yaw = -_yaw
         
             ## step 4 : append the current object to the 'objects' array
+            objects.append([1, x, y, 0.0, 1.50, w, l, yaw])
         
     #######
     ####### ID_S3_EX2 START #######   
