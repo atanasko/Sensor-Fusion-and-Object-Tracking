1bc3652d249124b87264fc196f0934021544f9b0
Atanasko Boris Mitrev <atanasko.mitrev@gmail.com>
2022-04-08 00:55:57 +0200

    Implement step2 for track management module

--- student/trackmanagement.py
+++ student/trackmanagement.py
@@ -26,6 +26,7 @@ class Track:
     '''Track class with state, covariance, id, score'''
     def __init__(self, meas, id):
         print('creating track no.', id)
+        self.score_step = 1. / params.window
         M_rot = meas.sensor.sens_to_veh[0:3, 0:3] # rotation matrix from sensor to vehicle coordinates
         
         ############
@@ -35,20 +36,32 @@ class Track:
         # - initialize track state and track score with appropriate values
         ############
 
-        self.x = np.matrix([[49.53980697],
-                        [ 3.41006279],
-                        [ 0.91790581],
-                        [ 0.        ],
-                        [ 0.        ],
-                        [ 0.        ]])
-        self.P = np.matrix([[9.0e-02, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 9.0e-02, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 6.4e-03, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 2.5e+03, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 2.5e+03, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 2.5e+01]])
-        self.state = 'confirmed'
-        self.score = 0
+        # transform measurement to vehicle coordinates
+        pos_sens = np.ones((4, 1))  # homogeneous coordinates
+        pos_sens[0:3] = meas.z[0:3]
+        pos_veh = meas.sensor.sens_to_veh * pos_sens
+
+        # save initial state from measurement
+        self.x = np.zeros((6, 1))
+        self.x[0:3] = pos_veh[0:3]
+
+        P_pos = M_rot * meas.R * np.transpose(M_rot)
+
+        # set up velocity estimation error covariance
+        sigma_p44 = 50  # initial setting for estimation error covariance P entry for vx
+        sigma_p55 = 50  # initial setting for estimation error covariance P entry for vy
+        sigma_p66 = 5  # initial setting for estimation error covariance P entry for vz
+        P_vel = np.matrix([[sigma_p44 ** 2, 0, 0],
+                           [0, sigma_p55 ** 2, 0],
+                           [0, 0, sigma_p66 ** 2]])
+
+        # overall covariance initialization
+        self.P = np.zeros((6, 6))
+        self.P[0:3, 0:3] = P_pos
+        self.P[3:6, 3:6] = P_vel
+
+        self.state = 'initialized'
+        self.score = self.score_step
         
         ############
         # END student code
@@ -91,6 +104,7 @@ class Trackmanagement:
         self.track_list = []
         self.last_id = -1
         self.result_list = []
+        self.score_step = 1. / params.window
         
     def manage_tracks(self, unassigned_tracks, unassigned_meas, meas_list):  
         ############
@@ -107,9 +121,14 @@ class Trackmanagement:
             if meas_list: # if not empty
                 if meas_list[0].sensor.in_fov(track.x):
                     # your code goes here
-                    pass 
+                    track.score -= self.score_step
 
-        # delete old tracks   
+        # delete old tracks
+        for track in self.track_list:
+            if (track.state == 'confirmed' and track.score <= params.delete_threshold) \
+                    or ((track.state == 'initialized' or track.state == 'tentative')
+                        and (track.P[0, 0] > params.max_P or track.P[1, 1] > params.max_P)):
+                self.delete_track(track)
 
         ############
         # END student code
@@ -139,8 +158,12 @@ class Trackmanagement:
         # - increase track score
         # - set track state to 'tentative' or 'confirmed'
         ############
-
-        pass
+        if track.score < 1:
+            track.score += self.score_step
+        if self.score_step < track.score < params.confirmed_threshold:
+            track.state = 'tentative'
+        if track.score >= params.confirmed_threshold:
+            track.state = 'confirmed'
         
         ############
         # END student code
